2db52889618bc5ade2642f38eeb9ab22336fee8a
Merd00 <merttozturkk52@gmail.com>
2026-03-29 02:09:29 +0300

    Environment variable eklendi, deploy hazırlığı

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 })
 
-// Her istekte token'ı otomatik ekle
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
@@ -13,7 +12,6 @@ api.interceptors.request.use((config) => {
   return config
 })
 
-// Token geçersizse login'e yönlendir
 api.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -26,3 +24,5 @@ api.interceptors.response.use(
 )
 
 export default api
+
+
